6feeab191bbf27b189e6751098b135ccc9945bbb
Russell Jurney <russell.jurney@gmail.com>
2011-06-11 00:40:22 -0700

    Bare bones for debug

--- index.rb
+++ index.rb
@@ -3,18 +3,20 @@ require 'sinatra'
 require 'erb'
 require 'json'
 
-require './mailgun'
-Mailgun::init("key-41q2py_zo0op3evcz7")
+#require './mailgun'
+#Mailgun::init("key-41q2py_zo0op3evcz7")
 
 get '/' do
   puts "Hello, worls!"
-  @params = 'foo'
-  erb :'index.html.erb'
+  "Hello, world!"
 end
+#  @params = 'foo'
+#  erb :'index.html.erb'
+#end
 
-post '/email/:data' do |data|
-  puts "We got a request, yo!"
-  @params = params
-  puts @params.inspect
-  puts "Data: #{data.inspect}"
-end
+#post '/email/:data' do |data|
+#   puts "We got a request, yo!"
+#   @params = params
+#   puts @params.inspect
+#   puts "Data: #{data.inspect}"
+# end
