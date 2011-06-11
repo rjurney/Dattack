62746fab978de2394918f1940adeeb2add7a437b
Russell Jurney <russell.jurney@gmail.com>
2011-06-10 23:53:02 -0700

    Initial stub app testing mailgun.

---
+++ index.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+require 'json'
+
+require 'mailgun'
+Mailgun::init("key-41q2py_zo0op3evcz7")
+
+post '/email' do
+  @params = params
+  erb :'index.html.erb'
+end
