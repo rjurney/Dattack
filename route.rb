0b22e09861a38aa3db6012db92d1c4be186c8240
Russell Jurney <russell.jurney@gmail.com>
2011-06-11 00:10:10 -0700

    Bundler setup.

--- route.rb
+++ route.rb
@@ -1,5 +1,5 @@
 require 'mailgun'
 Mailgun::init("key-41q2py_zo0op3evcz7")
 
-route = Route.new(:pattern => 'helper@dattack.mailgun.org', :destination => 'http://dattack.heroku.com/email')
+route = Route.new(:pattern => 'helper@dattack.mailgun.org', :destination => 'http://fierce-moon-207.heroku.com//email')
 route.upsert()
