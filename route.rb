ee66d77a95f45fdf91622a032867b01abdf41662
Russell Jurney <russell.jurney@gmail.com>
2011-06-18 17:45:31 -0700

    foo

--- route.rb
+++
@@ -1,5 +0,0 @@
-require 'mailgun'
-Mailgun::init("key-41q2py_zo0op3evcz7")
-
-route = Route.new(:pattern => 'helper@dattack.mailgun.org', :destination => 'http://fierce-moon-207.heroku.com//email')
-route.upsert()
