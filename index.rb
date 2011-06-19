f11d97a637564d7753c2f394ae2a35caeb25e3e4
Russell Jurney <russell.jurney@gmail.com>
2011-06-18 18:58:53 -0700

    Added SQS queueing for emails

--- index.rb
+++ index.rb
@@ -7,6 +7,8 @@ require 'right_aws'
 require 'lib/mailgun'
 Mailgun::init("key-41q2py_zo0op3evcz7")
 
+sqs = RightAws::SqsGen2.new(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+
 get '/' do
   " "
 end
@@ -15,5 +17,23 @@ post '/email' do
   puts "ID is: #{params[:subject]}"
   puts "Incoming Email Post: "
   params.each {|key, value| puts "Key: #{key} Value: #{value}"}
+  
+  sqs.put parse_email params
+
   "true"
+end
+
+def parse_email(email)
+  return JSON {
+                :received => email['Received'],
+                :message_id => email['Message-Id'],
+                :recipient => email['recipient'],
+                :from => email['from'], 
+                :to => email['to'], 
+                :sender => email['sender'],
+                :subject => email['subject'], 
+                :body => email['stripped-text'],
+                :'stripped-html' => email['stripped-html'],
+                :'body-plain' => email['body-plain']
+              }
 end
