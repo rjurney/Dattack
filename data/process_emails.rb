ee9675e935f23ad6bcd7c111270b371fd160583f
Russell Jurney <russell.jurney@gmail.com>
2011-06-18 21:02:54 -0700

    Added uuids for redis lookup of emails

--- data/process_emails.rb
+++ data/process_emails.rb
@@ -3,7 +3,7 @@ require 'erb'
 require 'json'
 require 'right_aws'
 require 'redis'
-require 'system_timer'
+require 'uuid'
 
 while(1) {
   
--- index.rb
+++ index.rb
@@ -4,7 +4,7 @@ require 'erb'
 require 'json'
 require 'right_aws'
 require 'redis'
-require 'system_timer'
+require 'uuid'
 
 sqs = RightAws::SqsGen2.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 queue = RightAws::SqsGen2::Queue.new(sqs, 'kontexa_test')
@@ -13,16 +13,19 @@ queue.clear() # Dev only!
 redis_uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
 
+uuid_factory = UUID.new
+
 get '/' do
   " "
 end
 
 post '/email' do
-  puts "ID is: #{params[:subject]}"
-  puts "Incoming Email Post: "
+  puts "Incoming Email Post:"
   params.each {|key, value| puts "Key: #{key} Value: #{value}"}
   
-  queue.push JSON params
+  uuid = uuid_factory.generate
+  redis.put uuid, JSON(params)
+  queue.push uuid
 
   "true"
 end
