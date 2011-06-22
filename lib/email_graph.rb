b0f5a411cd16ca925576115a699763853e39ec53
Russell Jurney <russell.jurney@gmail.com>
2011-06-21 18:41:01 -0700

    find_or_increment_edge works.  unit tests done.

--- lib/email_graph.rb
+++ lib/email_graph.rb
@@ -7,7 +7,7 @@ class EmailGraph < Pacer::TinkerGraph
   # Example Usage: cc = find_or_create_vertex {:type => 'email', :address => email}, :type
   #
   def find_or_create_vertex(properties, key)
-    if self.v(key =>  properties[key]).empty?
+    if self.v(key => properties[key]).empty?
       vertex = self.create_vertex(properties)
     else
       vertex = self.v(key => properties[key]).first
@@ -16,10 +16,19 @@ class EmailGraph < Pacer::TinkerGraph
   
   # If an edge exists, increment it.  Otherwise create it and then increment it.
   #
-  # Example Usage: graph.find_or_increment_edge nil, from, cc, 'sent', {volume => 1}
+  # Example Usage: graph.find_or_increment_edge from, cc, :sent, {volume => 1}
   #
-  def find_or_increment_edge(from, to, key, amount)
-    # Check for an existing edge with a traversal
-    summary_graph.create_edge nil, vertices[sender], vertices[recipient], :sent, {:volume => volume}
+  def find_or_increment_edge(from, to, type, key, amount)
+    # Does the edge exist?
+    edge = from.out_e type
+    edge &&= edge.first
+    
+    if edge.nil?
+      self.create_edge nil, from, to, type, {key.to_s => amount}
+    # If so, increment the field
+    else
+      edge[key.to_s] += amount
+      edge
+    end
   end
 end
--- spec/email_graph.rb
+++ spec/email_graph.rb
@@ -1,15 +1,15 @@
 require 'lib/email_graph'
-
-# Setup
-graph = EmailGraph.new
-properties1 = {:type => 'email', :address => 'russell.jurney@gmail.com'}
-properties2 = {:type => 'email', :address => 'kate.jurney@gmail.com'}
+require 'rspec'
 
 describe EmailGraph, "#find_or_create_vertex" do
+  # Setup
+  graph = EmailGraph.new
+  properties1 = {:type => 'email', :address => 'russell.jurney@gmail.com'}
+  properties2 = {:type => 'email', :address => 'kate.jurney@gmail.com'}
+  
   vertex1 = nil
   it "creates a new vertex if none is found to match its index field" do
-    # Create a vertex
-    vertex1 = graph.find_or_create_vertex properties1, :type
+    vertex1 = graph.find_or_create_vertex properties1, :address
     vertex1[:type].should == 'email'
     vertex1[:address].should == 'russell.jurney@gmail.com'
     graph.v(:address => 'russell.jurney@gmail.com').first.should === vertex1
@@ -17,7 +17,31 @@ describe EmailGraph, "#find_or_create_vertex" do
   
   it "returns the existing vertex if one is found that matches its index field" do
     # Get the existing vertex
-    vertex2 = graph.find_or_create_vertex properties2, :type
+    vertex2 = graph.find_or_create_vertex properties1, :address
     vertex2.should === vertex1
+  end
+end
+
+describe EmailGraph, "#find_or_increment_edge" do
+  # Setup
+  graph = EmailGraph.new
+  properties1 = {:type => 'email', :address => 'russell.jurney@gmail.com'}
+  properties2 = {:type => 'email', :address => 'kate.jurney@gmail.com'}
+  
+  from = graph.create_vertex properties1
+  to = graph.create_vertex properties2
+  edge1 = nil
+  
+  it "should create a new edge when none is present" do
+    edge1 = graph.find_or_increment_edge(from, to, 'sent', 'volume', 1)
+    edge1.out_v.first.should === from
+    edge1.in_v.first.should === to
+    edge1['volume'].should === 1
+  end
+  
+  it "should increment the value of the key in an existing edge" do
+    edge2 = graph.find_or_increment_edge(from, to, 'sent', 'volume', 4)
+    edge2['volume'].should === 5
+    edge2.should === edge1
   end
 end
