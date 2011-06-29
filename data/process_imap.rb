005632199f96513755e63859c0f6a7edd5b1de17
Russell Jurney <russell.jurney@gmail.com>
2011-06-28 19:35:46 -0700

    Removed mailbox redundancy via a loop, and am now populating graphs via IMAP.  I think.

--- data/process_imap.rb
+++ data/process_imap.rb
@@ -27,102 +27,74 @@ imap = Net::IMAP.new('imap.gmail.com',993,true)
 imap.login(USERNAME, ENV['GMAILPASS'])
 
 # First check the OUTBOX
-imap.examine('[Gmail]/Sent Mail') # examine is read only
-imap.search(['ALL']).each do |message_id|
-  
-  # Trap ctrl-c 
-  if interrupted
-    puts "Saving..."
-    system 'rm /tmp/historic_email.graphml'
-    hist_graph.export '/tmp/historic_email.graphml'
-    graph_client.set USERKEY, hist_graph
-    exit
-  end
-  
-  msg = imap.fetch(message_id,'RFC822')[0].attr['RFC822']
-  mail = TMail::Mail.parse(msg)
-  from_addresses = mail.header['from'].addrs
-  to_addresses = mail.header['to'].addrs
-  
-  from_addresses.each do |from|
-    to_addresses.each do |to|
-      puts "#{from.address} --> #{to.address} [to]"
-    end
-  
-    if mail.header['cc']
-      cc_addresses = mail.header['cc'].addrs
-      cc_addresses.each do |cc|
-        puts "#{from.address} --> #{cc.address} [cc]"
-      end
-    end
-    
-    if mail.header['bcc']
-      bcc_addresses = mail.header['bcc'].addrs
-      bcc_addresses.each do |bcc|
-        puts "#{from.address} --> #{bcc.address} [bcc]"
-      end
-    end
-  end
-  
-  if (count % 100) == 0
-    puts "Saving..."
-    system 'rm -f /tmp/historic_email.graphml'
-    hist_graph.export '/tmp/historic_email.graphml'
-    puts hist_graph
-    graph_client.set USERKEY, hist_graph
-  end
-  count += 1
-  
-end
 
-# Now check the INBOX
-count = 0
-
-imap.examine('INBOX') # examine is read only
-imap.search(['ALL']).each do |message_id|
+('[Gmail]/Sent Mail', 'INBOX').each do |folder|
+  imap.examine(folder) # examine is read only
+  imap.search(['ALL']).each do |message_id|
   
-  # Trap ctrl-c 
-  if interrupted
-    puts "Saving..."
-    system 'rm /tmp/historic_email.graphml'
-    hist_graph.export '/tmp/historic_email.graphml'
-    graph_client.set USERKEY, hist_graph
-    exit
-  end
-  
-  msg = imap.fetch(message_id,'RFC822')[0].attr['RFC822']
-  mail = TMail::Mail.parse(msg)
-  from_addresses = mail.header['from'].addrs
-  to_addresses = mail.header['to'].addrs
-  
-  from_addresses.each do |from|
-    to_addresses.each do |to|
-      puts "#{from.address} --> #{to.address} [to]"
+    # Trap ctrl-c 
+    if interrupted
+      puts "Saving..."
+      system 'rm /tmp/historic_email.graphml'
+      hist_graph.export '/tmp/historic_email.graphml'
+      graph_client.set USERKEY, hist_graph
+      exit
     end
   
-    if mail.header['cc']
-      cc_addresses = mail.header['cc'].addrs
-      cc_addresses.each do |cc|
-        puts "#{from.address} --> #{cc.address} [cc]"
-      end
-    end
+    msg = imap.fetch(message_id,'RFC822')[0].attr['RFC822']
+    mail = TMail::Mail.parse(msg)
+    from_addresses = mail.header['from'].addrs
+    to_addresses = mail.header['to'].addrs
+  
+    from_addresses.each do |t_from|
+      from = hist_graph.find_or_create_vertex({:type => 'email', :address => t_from.address}, :address)
     
-    if mail.header['bcc']
-      bcc_addresses = mail.header['bcc'].addrs
-      bcc_addresses.each do |bcc|
-        puts "#{from.address} --> #{bcc.address} [bcc]"
+      to_addresses.each do |t_to|
+        to = hist_graph.find_or_create_vertex({:type => 'email', :address => t_to.address}, :address)
+        edge = hist_graph.find_or_create_edge(from, to, 'sent')
+        props = edge.properties || {}
+        props.merge!({ 'volume' => ((props['volume'].to_i || 0) + 1).to_s })
+        edge.properties = props
+
+        puts "#{t_from.address} --> #{t_to.address} [to]"
+      end
+  
+      if mail.header['cc']
+        cc_addresses = mail.header['cc'].addrs
+        cc_addresses.each do |t_cc|
+          cc = graph.find_or_create_vertex({:type => 'email', :address => t_cc.address}, :address)
+          edge = graph.find_or_create_edge(from, cc, 'sent')
+          props = edge.properties || {}
+          props.merge!({ 'volume' => ((props['volume'].to_i || 0) + 1).to_s })
+          edge.properties = props
+        
+          puts "#{t_from.address} --> #{t_cc.address} [cc]"
+        end
+      end
+    
+      if mail.header['bcc']
+        bcc_addresses = mail.header['bcc'].addrs
+        bcc_addresses.each do |t_bcc|
+          bcc = graph.find_or_create_vertex({:type => 'email', :address => t_bcc.address}, :address)
+          edge = graph.find_or_create_edge(from, bcc, 'sent')
+          props = edge.properties || {}
+          props.merge!({ 'volume' => ((props['volume'].to_i || 0) + 1).to_s })
+          edge.properties = props          
+          
+          puts "#{t_from.address} --> #{to_bcc.address} [bcc]"
+        end
       end
     end
-  end
   
-  if (count % 100) == 0
-    puts "Saving..."
-    system 'rm -f /tmp/historic_email.graphml'
-    hist_graph.export '/tmp/historic_email.graphml'
-    puts hist_graph
-    graph_client.set USERKEY, hist_graph
+    if (count % 100) == 0
+      puts "Saving..."
+      system 'rm -f /tmp/historic_email.graphml'
+      hist_graph.export '/tmp/historic_email.graphml'
+      puts hist_graph
+      graph_client.set USERKEY, hist_graph
+    end
+    count += 1
   end
-  count += 1
 end
 
 def check_email_addresses(addresses)
