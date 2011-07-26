3ceaab79748a02b53bff252295d8d13c0bd7f144
Russell Jurney <russell.jurney@gmail.com>
2011-07-25 23:54:31 -0700

    Have TF/IDF on email documents working, minus the df() function to get a reverse index from voldemort.  Consider not using Voldemort, but using Lucene or something?

--- lib/email_summary.rb
+++ lib/email_summary.rb
@@ -11,77 +11,83 @@ class EmailSummary
       unless(ENV['VOLDEMORT_STORE'] && ENV['VOLDEMORT_ADDRESS'])
     @subject = subject
     @body = body
-    @stop_words = self.load_stop_words
-    @dict = self.load_dict
+    self.load_stop_words
+    self.load_dict
     @voldemort = VoldemortClient.new ENV['VOLDEMORT_STORE'], ENV['VOLDEMORT_ADDRESS']
   end
   
   def load_stop_words
-    @words = []
+    @stop_words = []
     file = File.new("data/english.stop", "r")
     while(line = file.gets)
-      @words << line.chop
+      @stop_words << line.chop
     end
     file.close
-    @words
   end
   
   def load_dict
-    dict = []
+    @dict = []
     file = File.new("/usr/share/dict/words", "r")
     while(line = file.gets)
-      dict << line.chop
+      @dict << line.chop
     end
-    dict
   end
   
   def summarize
     self.to_terms
-    
+    tfs, magnitude = self.to_tf_idf
+    puts tfs.inspect
+    puts magnitude
   end
 
   def to_terms(terms={}, boost=1)
     # remove all non letters and reject stop words
     terms_list = @body.gsub(/(\s|\d|\W)+/u,' ').rstrip.strip.downcase.split(' ').reject{|term|@stop_words.include?(term)}
+
     # transform to a hash with a frequency * boost value
-    
     terms_list.each do |term|
       # If its in the dictionary, stem it.  Otherwise leave it be, presumably its a technical term/jargon.
       if @dict.include?(term)
         term = term.stem if term.stem
       end
       if terms[term]
-        terms[term] = terms[term] + boost
+        terms[term] = terms[term] + boost.to_f
       else
-        terms[term] = boost
+        terms[term] = boost.to_f
       end
     end
     @terms = terms
   end 
 
   def to_tf_idf
-    #assume we have a method that returns the df value for any term
-    total_frequency = terms.values.inject(0){|a,b|a+b}
-    terms.each do |term,freq| 
-      terms[term]= (freq / total_frequency) * self.df(term)
-      magnitude = magnitude + terms[term]**2
+    tfs = {}
+    magnitude = 0
+    total_frequency = @terms.values.inject(0){|a,b|a+b}
+    puts "\nTotal frequency: #{total_frequency}"
+    @terms.each do |term,freq|
+      puts "Term: #{term}, Freq: #{freq}"
+      tfs[term]= (freq / total_frequency) * (self.df(term) || 1)
+      puts tfs[term]
+      magnitude = magnitude + tfs[term]**2
     end
-    return terms, magnitude
+    return tfs, magnitude
   end
-  
+
+  #assume we have a method that returns the df value for any term
   def df(term)
-    @voldemort.get(PREFIX + term)
+    1.0
+    #@voldemort.get(PREFIX + term) || 1
   end
   
-  def match(item)
-    my_tf_idf,  my_magnitude  = self.to_tf_idf
-    his_tf_idf, his_magnitude = item.to_tf_idf
-    dot_product = 0
-    my_tf_idf.each do |term,tf_idf|
-      dot_product = dot_product + tf_idf * his_tf_idf[term] if his_tf_idf[term]
-    end
-    cosine_similarity = dot_product / Math.sqrt(my_magnitude * his_magnitude)
-  end
+  # def match(item)
+  #   my_tf_idf,  my_magnitude  = self.to_tf_idf
+  #   his_tf_idf, his_magnitude = item.to_tf_idf
+  #   dot_product = 0
+  #   my_tf_idf.each do |term,tf_idf|
+  #     dot_product = dot_product + tf_idf * his_tf_idf[term] if his_tf_idf[term]
+  #   end
+  #   cosine_similarity = dot_product / Math.sqrt(my_magnitude * his_magnitude)
+  # end
 end
 
 class VoldemortException < Exception
--- spec/email_summary.rb
+++ spec/email_summary.rb
@@ -11,7 +11,9 @@ describe EmailSummary, "#new" do
     summarizer = EmailSummary.new subject, body
     summarizer.class.should == EmailSummary
   end
-  
+end
+
+describe EmailSummary, "#to_terms" do
   it "should produce a list of stems" do
     subject = 'Monitoring'
     body = "Greetings,
@@ -24,5 +26,35 @@ describe EmailSummary, "#new" do
     summarizer = EmailSummary.new subject, body
     summarizer.to_terms
     summarizer.terms.each_pair {|term, count| STDERR.write "#{term}: #{count.to_s}, " }
+    puts ""
+
+    # Two common words that don't get stemmed
+    summarizer.terms['monitoring'].should === 1
+    summarizer.terms['services'].should === 1
+    # Two jargon words that shouldn't get stemmed
+    summarizer.terms['hadoop'].should === 2
+    summarizer.terms['dfs'].should === 1
+    # Two common words that do get stemmed
+    summarizer.terms['advanc'].should === 1
+    summarizer.terms['notifi'].should === 1
+    # Two stop words that should not get through
+    summarizer.terms['the'].should === nil
+    summarizer.terms['and'].should === nil
+  end
+end
+
+describe EmailSummary, "#to_tf_idf" do
+  it "should do tf_idf" do
+    subject = 'Monitoring'
+    body = "Greetings,
+
+    I am relatively new to Hadoop but we now have an 10 node cluster up and running just DFS for now and will be expanding this rapidly as well as adding Hbase. I am looking to find out what people are using for monitoring Hadoop currently. 
+    I want to be notified if a node fails, performance statistics, failed drive or services ect. I was thinking of using Opsview and trying in Ganglia. Thanks in advance
+
+    Joe"
+    
+    summarizer = EmailSummary.new subject, body
+    summarizer.summarize
+    
   end
 end
