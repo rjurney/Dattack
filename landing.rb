ca739f81fde86e2842b8064516085d565439b33f
Russell Jurney <russell.jurney@gmail.com>
2011-08-01 14:02:46 -0700

    OAuth works.

--- landing.rb
+++ landing.rb
@@ -4,8 +4,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'json'
 require 'redis'
 require 'tmail'
+require 'uri'
+require 'oauth'
+require 'oauth/consumer'
 
 require 'jcode'
 $KCODE = 'UTF8'
@@ -17,22 +21,69 @@ redis_uri = URI.parse(ENV["REDISTOGO_URL"])
 redis = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
 
 set :public, File.dirname(__FILE__) + '/static'
+enable :sessions
+
+before do
+  session[:oauth] ||= {}
+  
+  consumer_key = ENV["CONSUMER_KEY"] || ENV["consumer_key"]
+  consumer_secret = ENV["CONSUMER_SECRET"] || ENV["consumer_secret"]
+  
+  @consumer ||= OAuth::Consumer.new(consumer_key, consumer_secret,
+    :site => "https://www.google.com",
+    :request_token_path => '/accounts/OAuthGetRequestToken?scope=https://mail.google.com/%20https://www.googleapis.com/auth/userinfo%23email',
+	  :access_token_path => '/accounts/OAuthGetAccessToken',
+	  :authorize_path => '/accounts/OAuthAuthorizeToken'
+  )
+  
+  if !session[:oauth][:request_token].nil? && !session[:oauth][:request_token_secret].nil?
+	  @request_token = OAuth::RequestToken.new(@consumer, session[:oauth][:request_token], session[:oauth][:request_token_secret])
+  end
+  
+  if !session[:oauth][:access_token].nil? && !session[:oauth][:access_token_secret].nil?
+	  @access_token = OAuth::AccessToken.new(@consumer, session[:oauth][:access_token], session[:oauth][:access_token_secret])
+  end
+end
 
 get "/" do
-  erb :landing
+  if @access_token
+    response = @access_token.get('https://www.googleapis.com/userinfo/email?alt=json')
+    if response.is_a?(Net::HTTPSuccess)
+      @email = JSON.parse(response.body)['data']['email']
+    else
+      STDERR.puts "could not get email: #{response.inspect}"
+    end
+    erb :authed
+  else
+    erb :landing
+  end
 end
 
 post "/signup" do
   email = params[:email]
-  if is_valid email
-    redis.set email, "1"
-    "Email: #{email}"
+  if is_valid? email
+    @request_token = @consumer.get_request_token(:oauth_callback => "#{request.scheme}://#{request.host}:#{request.port}/auth")
+    session[:oauth][:request_token] = @request_token.token
+    session[:oauth][:request_token_secret] = @request_token.secret
+    redirect @request_token.authorize_url
   else
-    "Invalid Email: #{email}"
+    redirect "/" #"Invalid Email: #{email}"
   end
 end
 
-def is_valid(email)
+get "/auth" do
+  @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier]
+  session[:oauth][:access_token] = @access_token.token
+  session[:oauth][:access_token_secret] = @access_token.secret
+  redirect "/"
+end
+
+# get "/logout" do
+#   session[:oauth] = {}
+#   redirect "/"
+# end
+
+def is_valid?(email)
   begin
     t_email = TMail::Address.parse email
     true
