524ac76599c36601fe7eade03ba17bf64d35b71c
Russell Jurney <russell.jurney@gmail.com>
2011-08-02 17:12:51 -0700

    Added google analytics to landing and authed via a partial ERB template, and added utility to print all redis keys

---
+++ bin/keys_redis.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env jruby
+
+require 'rubygems'
+require 'uri'
+require 'redis'
+
+# Warning - this deletes all keys from redis!
+redis_uri = URI.parse(ENV["REDISTOGO_URL"])
+redis = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
+
+(redis.keys '*').each {|key| value = redis.get key; puts "[ #{key} ]"}
